922304156cdc24a772211802a0dd11a3d7114d86
mr chapai <myicskt@gmail.com>
2024-02-20 15:01:54 -0600

    I added the schreenshot in project

--- src/main/java/org/DroupDownHandlemultiselect.java
+++ src/main/java/org/DroupDownHandlemultiselect.java
@@ -1,12 +1,15 @@
 package org;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -33,6 +36,17 @@ public class DroupDownHandlemultiselect {
 			Assert.assertTrue(false);
 		}else {
 			Assert.assertTrue(true);
+		}
+		try {
+			// generate file name based on time
+			String fileName = DateTimeFormatter.ofPattern("MM-dd-yyyy_hh-mm-ss").format(LocalDateTime.now());
+			TakesScreenshot sn = (TakesScreenshot) driver;
+			File file = sn.getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(file, new File("screenshots/"  + fileName + ".png"));
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 		driver.quit();
